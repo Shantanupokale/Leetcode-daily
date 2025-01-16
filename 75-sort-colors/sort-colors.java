931b96d65281802219fccb4eb8e87a8935eb9593
Shantanupokale <120240452+Shantanupokale@users.noreply.github.com>
2025-01-16 20:20:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (42.44%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        // Count occurrences of 0, 1, and 2
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count0++;
+            } else if (nums[i] == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        // Fill the array with the counts
+        int index = 0;
+
+        // Fill 0s
+        for (int i = 0; i < count0; i++) {
+            nums[index++] = 0;
+        }
+
+        // Fill 1s
+        for (int i = 0; i < count1; i++) {
+            nums[index++] = 1;
+        }
+
+        // Fill 2s
+        for (int i = 0; i < count2; i++) {
+            nums[index++] = 2;
+        }
+    }
+}
