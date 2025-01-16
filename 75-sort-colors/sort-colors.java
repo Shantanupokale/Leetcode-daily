84a6af4ddee9492de59e4278bdfab24b1d724b86
Shantanupokale <120240452+Shantanupokale@users.noreply.github.com>
2025-01-16 20:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.16%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,39 +1,27 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
+        int low = 0, high = nums.length - 1, current = 0;
 
-        // Count occurrences of 0, 1, and 2
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                count0++;
-            } else if (nums[i] == 1) {
-                count1++;
+        while (current <= high) {
+            if (nums[current] == 0) {
+                // Swap nums[current] and nums[low]
+                int temp = nums[current];
+                nums[current] = nums[low];
+                nums[low] = temp;
+
+                low++;
+                current++;
+            } else if (nums[current] == 2) {
+                // Swap nums[current] and nums[high]
+                int temp = nums[current];
+                nums[current] = nums[high];
+                nums[high] = temp;
+
+                high--;
             } else {
-                count2++;
+                // nums[current] is 1, just move current
+                current++;
             }
-        }
-
-        // Fill the array with the counts
-        int index = 0;
-
-        //Fill 0s
-        for (int i = 0; i < count0; i++) {
-        nums[index] = 0;
-        index++;
-        }
-
-        // Fill 1s
-        for (int i = 0; i < count1; i++) {
-            nums[index] = 1;
-            index++;
-        }
-
-        // Fill 2s
-        for (int i = 0; i < count2; i++) {
-            nums[index] = 2;
-            index++;
         }
     }
 }
