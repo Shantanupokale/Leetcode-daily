573f7ec1bd100915e80f954e7260a371abcb7220
Shantanupokale <120240452+Shantanupokale@users.noreply.github.com>
2025-01-18 19:22:23 +0530

    Time: 3 ms (100.00%) | Memory: 79.3 MB (22.63%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -2,7 +2,11 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int positiveIndex=0;
         int negativeIndex = 1 ;
-        int arr[] = new int[nums.length];
+        int arr[] = new int[nums.length];   
+        
+         //create new arr[] and put positive index with postive 
+        //and neagtive index with negative values
+        
         for (int i = 0 ; i< nums.length ; i ++){
             if(nums[i] > 0){ 
                 arr[positiveIndex]= nums[i];
